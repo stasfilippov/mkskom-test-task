73968d707d1fb32f6a8ddc27178c5fd2b2a5614a
stasfilippov <115743399+stasfilippov@users.noreply.github.com>
2023-05-27 18:46:48 +0300

    add: create-repository

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <h1>Hello</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyHeader',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MyHeader msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import MyHeader from '@/components/MyHeader.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    MyHeader
   }
 }
 </script>
