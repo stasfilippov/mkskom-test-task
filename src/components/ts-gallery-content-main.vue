a38c7b8592b7161913284e9a3cee67286c25bf9d
stasfilippov <kron888999@gmail.com>
2023-06-08 00:18:36 +0300

    add: static-data-flex

--- src/components/ts-gallery-content-main.vue
+++ src/components/ts-gallery-content-main.vue
@@ -1,51 +1,68 @@
 <script>
 import { defineComponent } from "vue";
-import tsGalleryItem from "@/components/ts-gallery-item.vue";
+// import tsGalleryItem from "@/components/ts-gallery-item.vue";
 
 export default defineComponent({
   name: "ts-gallery-content-main",
-  components: { tsGalleryItem },
-  data() {
-    return {
-      items: [
-        {
-          albumId: 1,
-          id: 1,
-          title: "accusamus beatae ad facilis cum similique qui sunt",
-          url: "https://via.placeholder.com/600/92c952",
-          thumbnailUrl: "https://via.placeholder.com/150/92c952",
-        },
-        {
-          albumId: 1,
-          id: 2,
-          title: "reprehenderit est deserunt velit ipsam",
-          url: "https://via.placeholder.com/600/771796",
-          thumbnailUrl: "https://via.placeholder.com/150/771796",
-        },
-        {
-          albumId: 1,
-          id: 3,
-          title: "officia porro iure quia iusto qui ipsa ut modi",
-          url: "https://via.placeholder.com/600/24f355",
-          thumbnailUrl: "https://via.placeholder.com/150/24f355",
-        },
-        {
-          albumId: 1,
-          id: 4,
-          title: "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
-          url: "https://via.placeholder.com/600/d32776",
-          thumbnailUrl: "https://via.placeholder.com/150/d32776",
-        },
-      ],
-    };
+  components: {},
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
   },
 });
 </script>
 
 <template>
   <div class="ts-gallery-content__main">
-    <ts-gallery-item />
+    <div class="ts-gallery-item" :key="item.id" v-for="item in items">
+      <div class="ts-gallery-item__image-box">
+        <img :src="item.url" alt="#" class="ts-gallery-item__image" />
+      </div>
+      <h4 class="ts-gallery-item__title">{{ item.title }}</h4>
+      <p class="ts-gallery-item__date">28.11.2045</p>
+    </div>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@import "@/assets/layouts/common/mixins";
+
+.ts-gallery-item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 160px;
+
+  &__image-box {
+    border: 2px solid $borderColor;
+    box-shadow: 0 2px 5px rgba(38, 51, 77, 0.03);
+    border-radius: 10px;
+    padding: 5px;
+    width: 160px;
+    height: 120px;
+    margin-bottom: 20px;
+  }
+
+  &__image {
+    border-radius: 5px;
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+  }
+
+  &__title {
+    @include font($colorTextDark, 12px, 700, 20px);
+    margin-bottom: 10px;
+    text-align: center;
+    max-width: 122px;
+    max-height: 20px;
+    overflow: hidden;
+  }
+
+  &__date {
+    @include font($colorTextSecond, 10px, 900, 20px);
+  }
+}
+</style>
